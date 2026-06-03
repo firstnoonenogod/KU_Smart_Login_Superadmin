b38955345a63878a1034e3e066bfae0608ac42fb
Macbook <admin@MacBook-Air-khxng-Macbook-2.local>
2026-06-03 17:13:07 +0700

    fix graph display mistake

--- server.js
+++ server.js
@@ -744,6 +744,7 @@ app.get('/api/dashboard/org/:id', requireAuth, async (req, res) => {
                     u.username, 
                     c.create_time as created_at, 
                     c.expire_time,
+                    c.issued_by,
                     COALESCE(o.org_name, 'องค์กรที่ถูกลบไปแล้ว') as org_name
                 FROM wifi_users u
                 JOIN wifi_credentials c ON u.id = c.user_id
